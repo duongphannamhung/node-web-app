d0a4bc4dfb416e8ee532eeb9b6ab519458e5dcb9
hung.duong <hung.duong@be.com.vn>
2023-12-23 21:17:53 +0700

    search product

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,11 +1,14 @@
 let controller = {};
 
 const models = require('../models');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
 
 controller.show = async (req, res) => {
     let category = isNaN(req.query.category) ? 0 : parseInt(req.query.category);
     let brand = isNaN(req.query.brand) ? 0 : parseInt(req.query.brand);
     let tag = isNaN(req.query.tag) ? 0 : parseInt(req.query.tag);
+    let keyword = req.query.keyword || '';
 
     let categories = await models.Category.findAll({
         include: [{ model: models.Product }]
@@ -37,6 +40,12 @@ controller.show = async (req, res) => {
             model: models.Tag,
             where: { id: tag },
         }]
+    }
+
+    if (keyword.trim() != '') {
+        options.where.name = {
+            [Op.iLike]: `%${keyword}%`
+        }
     }
 
     let products = await models.Product.findAll(options);
