81739bfefdd798b5b0ca6d42b45e8fb5cd00518e
hung.duong <hung.duong@be.com.vn>
2023-12-25 23:09:19 +0700

    feat-28/show-cart

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -6,6 +6,8 @@ let controller = require('../controllers/productsController');
 let cartController = require('../controllers/cartController');
 
 router.get('/', controller.show);
+
+router.get('/cart', cartController.show);
 router.get('/:id', controller.showDetails);
 
 router.post('/cart', cartController.add);
